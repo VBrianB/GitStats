3d14811f4cffdb66cf6f05a46e81821b4d8ae089
VBrianB <viniciusbbolzanidev@gmail.com>
2022-08-25 13:58:01 -0300

    create 2 screens and fix bugs

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 
 
-async function pegardados(){
 
-try{    
-        let User = 'VBrianB';
+let User = 'maykbrito';
+
+
+document.querySelector('.btn-start-git').addEventListener('click', (e)=>{
+    let ScreenW = document.querySelector('.container-welcome');
+    let InputScreen = document.querySelector('.input-screen');
+ 
+    animationOut(ScreenW);
+    setTimeout(()=>{
+        animationIn(InputScreen);
+    },1200)
+    
+    console.log('executou');
+});
+
+
+
+document.querySelector('.btn-start-git-stats').addEventListener('click', ()=>{
+
+    let InputScreen = document.querySelector('.input-screen');
+    let StatsScreen = document.querySelector('.container');
+    animationOut(InputScreen);
+    setTimeout(()=>{
+        StatsScreen.style.display = 'flex'
+        setTimeout(()=>{
+            StatsScreen.style.opacity = '1'
+        },300)
+    },1200)
+    pegardados();
+
+
+});
+
+
+
+async function pegardados() {
+
+    try {
+        let User = document.querySelector('#user').value;
         const response = await fetch(`https://api.github.com/users/${User}`)
-   
-       
-        const user = await response.json();
-        
 
+
+        const user = await response.json();
+
+       
         // Interface - Profile
 
         let profilePic = document.getElementById('profile-img').src = await user.avatar_url;
@@ -19,8 +55,8 @@ try{
         let Localization = document.getElementById('localization').textContent = await user.location;
         let Blog = document.getElementById('blog').href = await user.blog;
         let blogText = await user.blog
-        if(blogText.length > 25){
-            blogText = blogText.substring(0, 25)+'...'
+        if (blogText.length > 25) {
+            blogText = blogText.substring(0, 25) + '...'
         }
         let BlogText = document.getElementById('blog').innerHTML = blogText;
 
@@ -33,12 +69,12 @@ try{
         let imgLanguages = document.getElementById('stats-img-languages').src = `https://github-readme-stats.vercel.app/api/top-langs/?username=${User}&layout=compact&langs_count=7&theme=radical`;
         let ProfileViews = document.getElementById('profile_views').src = `https://komarev.com/ghpvc/?username=${User}&color=red`;
         let company = await user.company;
-        if(company !== null){
+        if (company !== null) {
             document.getElementById('company_h3').textContent = company
         }
         let ReadMe = document.getElementById('README_').src = `https://github-readme-stats.vercel.app/api/pin/?username=${User}&repo=${User}&theme=radical`
         let LinkRead = document.getElementById('link_read').href = `https://github.com/${User}/${User}`
-        
+        console.log('executou');
     }
 
 
@@ -46,10 +82,40 @@ try{
 
 
 
-catch(e){
-    console.log(e)
+    catch (e) {
+        console.log(e)
+    }
+
+
 }
 
-    
+
+
+
+/* Pegar Followers - Usar Map
+async function pegarFollowers(){
+        const response = await fetch(`https://api.github.com/users/${User}/followers`);
+        const Followers = await response.json();
+        console.log(Followers[0])
 }
-pegardados();
+pegarFollowers();*/
+
+
+
+
+function animationOut(item){
+    
+        item.style.opacity = '0'
+        setTimeout(()=>{
+            item.style.display = 'none'
+        },200)
+}
+function animationIn(item){
+   
+
+    item.style.display = 'block'
+    setTimeout(()=>{
+        item.style.opacity = '1'
+    },300)
+
+}
